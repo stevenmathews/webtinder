6ee4a3980db930a67cc56e322dc7cf96f7f36943
Steve Mathews <stevenmathews.nz@gmail.com>
2015-05-04 13:53:10 +1200

    get recommendations from tinder api function created

--- server/tinder.js
+++ server/tinder.js
@@ -1,2 +1,15 @@
 var tinder = Meteor.npmRequire('tinderjs');
 var client = new tinder.TinderClient();
+var currentUserFacebookId = Meteor.users.find().fetch()[0].services.facebook.id
+
+// Authorize function must be called before each action
+function getRecommendations (token, callback) {
+  client.authorize(token, currentUserFacebookId, function () {
+    client.getRecommendations(10, function (err, data) {
+      if (err) {
+        return callback(err)
+      }
+      callback(null, data.results)
+    });
+  });
+}
