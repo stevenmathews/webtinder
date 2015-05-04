ee65e4b1f56dae5c807962dcffc20d3b454ec9aa
Steve Mathews <stevenmathews.nz@gmail.com>
2015-05-04 13:55:13 +1200

    getRecommendations async now available to client

--- server/tinder.js
+++ server/tinder.js
@@ -3,7 +3,7 @@ var client = new tinder.TinderClient();
 var currentUserFacebookId = Meteor.users.find().fetch()[0].services.facebook.id
 
 // Authorize function must be called before each action
-function getRecommendations (token, callback) {
+function getRecommendations(token, callback) {
   client.authorize(token, currentUserFacebookId, function () {
     client.getRecommendations(10, function (err, data) {
       if (err) {
@@ -13,3 +13,11 @@ function getRecommendations (token, callback) {
     });
   });
 }
+
+var getRecommendationsSync = Async.wrap(getRecommendations);
+
+Meteor.methods({
+  getRecommendations: function(token) {
+    return getRecommendationsSync(token)
+  }
+});
