64fb7d5ed674069492231d60cf7e7cb0764e3ec7
Steve Mathews <stevenmathews.nz@gmail.com>
2015-05-04 15:38:05 +1200

    added age, distance and photos to recommendation template

---
+++ client/templates/recommendations/recommendation.js
@@ -0,0 +1,12 @@
+Template.recommendation.helpers({
+  dateToAge: function(dateOfBirth) {
+    var date = dateOfBirth.split("T")[0];
+    var age = moment().diff(date, 'years');
+    return age;
+  },
+  distance: function(distanceMi) {
+    var kmsPerMile = 1.60934
+    var kms = (distanceMi * kmsPerMile).toFixed(2);
+    return kms;
+  }
+});
