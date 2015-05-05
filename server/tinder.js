c1aa3315209535b14baa9ce9277651486e143790
Steve Mathews <stevenmathews.nz@gmail.com>
2015-05-05 14:00:00 +1200

    like function available on client

--- server/tinder.js
+++ server/tinder.js
@@ -23,8 +23,18 @@ function pass (tinderUserId, token, callback) {
   })
 }
 
+function like (tinderUserId, token, callback) {
+  client.authorize(token, currentUserFacebookId, function () {
+    client.like(tinderUserId, function (err, data) {
+      if (err) { return callback(err) }
+      callback(null, data)
+    })
+  })
+}
+
 var getRecommendationsSync = Async.wrap(getRecommendations);
 var passSync = Async.wrap(pass);
+var likeSync = Async.wrap(like);
 
 Meteor.methods({
   getRecommendations: function(token) {
@@ -35,6 +45,9 @@ Meteor.methods({
   },
   pass: function (tinderUserId, token) {
     return passSync(tinderUserId, token);
+  },
+  like: function (tinderUserId, token) {
+    return likeSync(tinderUserId, token);
   },
   remove: function (recommendationId) {
     return Recommendations.remove(recommendationId);
