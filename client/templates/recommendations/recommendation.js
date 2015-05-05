8d691fdaea58101f7b7ebc524cac57f1b752f5d9
Steve Mathews <stevenmathews.nz@gmail.com>
2015-05-05 14:03:47 +1200

    like event handler created

--- client/templates/recommendations/recommendation.js
+++ client/templates/recommendations/recommendation.js
@@ -30,5 +30,26 @@ Template.recommendation.events({
           }
         });
     });
+  },
+  'click #like': function () {
+    var recommendationId = this._id;
+    var token = Session.get('token');
+    Meteor.call("like", this.tinderUserId, token, function(err, res) {
+      if (err) { throw err }
+      if (res.match === false) {
+        console.log("No Match");
+      } else {
+        console.log("Match");
+      }
+      if (res)
+        Meteor.call("remove", recommendationId, function(err, res) {
+          if (err) { throw err }
+          if (res === 1) {
+            console.log('Liked');
+          } else {
+            console.log('Please Refresh Browser');
+          }
+        });
+    });
   }
 });
