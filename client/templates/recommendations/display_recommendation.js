2b3c0376e2cd45e6cdc4dd23cf03f1967ee77d65
Steve Mathews <stevenmathews.nz@gmail.com>
2015-05-06 12:56:31 +1200

    getRecommendations refactored

--- client/templates/recommendations/display_recommendation.js
+++ client/templates/recommendations/display_recommendation.js
@@ -8,24 +8,35 @@ Template.displayRecommendations.helpers({
   getRecommendations: function() {
     var token = Session.get('token');
     Meteor.call('getRecommendations', token, function (err, results) {
-      results.forEach(function(result) {
-        var recommendation = {
-          userId: Meteor.userId(),
-          tinderUserId: result._id,
-          bio: result.bio,
-          birthDate: result.birth_date,
-          commonFriendsCount: result.common_friend_count,
-          commonFriends: result.common_friends,
-          commonLikeCount: result.common_like_count,
-          commonLikes: result.common_likes,
-          connectionCount: result.connection_count,
-          distanceMi: result.distance_mi,
-          gender: result.gender,
-          name: result.name,
-          photos: result.photos,
-        }
-        Meteor.call('insertRecommendation', recommendation);
-      });
+      if (err) { throw err }
+      processResults(results);
+      insertProcessedResults(recommendations);
     });
   }
 });
+
+var recommendations = []
+
+function processResults(results) {
+  results.forEach(function(result) {
+    var recommendation = {
+      userId: Meteor.userId(),
+      tinderUserId: result._id,
+      bio: result.bio,
+      birthDate: result.birth_date,
+      commonFriends: result.common_friends,
+      commonLikes: result.common_likes,
+      connectionCount: result.connection_count,
+      distanceMi: result.distance_mi,
+      name: result.name,
+      photos: result.photos,
+    }
+    recommendations.push(recommendation)
+  });
+}
+
+function insertProcessedResults(recommendations) {
+  recommendations.forEach(function(recommendation) {
+    Meteor.call('insertRecommendation', recommendation);
+  });
+}
